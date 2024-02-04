9653352fec8d779de71e73eb9c05c7e4b46957bc
Zombox <ghostzb@outlook.com>
2024-02-04 18:28:14 +0700

    edit width

--- src/components/ShowTimer.tsx
+++ src/components/ShowTimer.tsx
@@ -9,11 +9,11 @@ type ShowTimerType = {
 function ShowTimer({timezoneData}:ShowTimerType) {
   const { timeNow, AMPM, dateNow } = useTime({timezoneData:timezoneData.timeZone});
   return (
-    <div className=" w-[36rem] flex justify-center flex-col">
+    <div className=" w-[36.5rem] flex justify-center flex-col">
       <div className="flex justify-end mr-2">
         <p className="text-4xl">{timezoneData.country}</p>
       </div>
-      <div className=" flex justify-between w-[36rem] my-2">
+      <div className=" flex justify-between w-[36.5rem] my-2">
         <p className="text-9xl">{timeNow}</p>
         <p className="text-9xl">{AMPM ? "PM" : "AM"}</p>
       </div>
