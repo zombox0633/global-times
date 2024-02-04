d361c41fc85bf75058832402fa875b68881c9c79
Zombox <ghostzb@outlook.com>
2024-02-04 18:27:24 +0700

    update design DropDown

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { timezoneData, timezoneType } from "../constraint/TIMEZONE_DATA";
 
 type DropDownType = {
@@ -5,17 +6,21 @@ type DropDownType = {
 };
 
 function DropDown({ setTimezone }: DropDownType) {
+  const [showDropDown, setShowDropDown] = useState<boolean>(false);
   return (
     <div>
-      <button className="focus__df flex items-center justify-center w-40 pl-4">
+      <button
+        className="focus__df flex items-center justify-center w-40 pl-4"
+        onClick={() => setShowDropDown((prev) => !prev)}
+      >
         TimeZone
-        <span className="material-symbols-outlined mt-2 ml-1">expand_more</span>
+        <span className={`material-symbols-outlined  ${showDropDown ? "rotate-0" : "rotate-180"} my-2 ml-1 transition-all duration-500 ease-in-out`}>chevron_left</span>
       </button>
-      <ul className="w-40 mt-4 ">
+      <ul className={`absolute ${showDropDown ? "left-12 ": "-left-[100%]"}  w-40 mt-4 transition-all duration-500 ease-in-out`}>
         {timezoneData.map((item, index) => (
           <li id={`timezone-${index + 1}`} className="mb-1 ">
             <button
-              className="pl-4 text-start w-40"
+              className="pl-4 text-start w-[9.75rem]"
               value={item.timeZone}
               onClick={() =>
                 setTimezone({ country: item.country, timeZone: item.timeZone })
