90d676ac345769463cd4f9fa8db332879448c98f
Zombox <ghostzb@outlook.com>
2024-02-15 20:28:32 +0700

    edit useParams and useFindTimezone

--- src/components/container/ContainerDateTime.tsx
+++ src/components/container/ContainerDateTime.tsx
@@ -1,10 +1,17 @@
+import { useParams } from "react-router-dom";
 import DisplayGlobalDate from "./DisplayGlobalDate";
 import DisplayGlobalTime from "./DisplayGlobalTime";
 import DisplayCountry from "./DisplayCountry";
+import { useDateTimeContext } from "../../context/TimeStampProvider";
+import useFindTimezone from "../../hook/useFindTimezone";
 
 function ContainerDateTime() {
+  const { countryPath } = useParams();
+  const { setTimezone } = useDateTimeContext();
+  useFindTimezone({ countryPath, setTimezone });
+
   return (
-    <div className=" w-56 sm:w-[20.5rem] lg:w-[27rem] 2xl:w-[36.5rem] flex justify-center flex-col">
+    <div className=" w-[13.7rem] sm:w-[20.7rem] lg:w-[27.5rem] 2xl:w-[36.5rem] flex justify-center flex-col">
       <DisplayCountry />
       <DisplayGlobalTime />
       <DisplayGlobalDate />
