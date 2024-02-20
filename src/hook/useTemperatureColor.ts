35ca0b7f1d12126a95dc5e1b92d8b570dbed4d33
Zombox <ghostzb@outlook.com>
2024-02-20 21:56:40 +0700

    edit switch to mapping

--- src/hook/useTemperatureColor.ts
+++ src/hook/useTemperatureColor.ts
@@ -1,69 +1,44 @@
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 type UseTemperatureColorPropsType = {
   tempData: number;
 };
 
+type TemperatureRangesType = {
+  max?: number;
+  min?: number;
+  color: string;
+};
+
 function useTemperatureColor({
   tempData,
 }: UseTemperatureColorPropsType): string {
-  const [tempColor, setTempColor] = useState<string>("bg-[#ffff]");
+  const tempColor = useMemo(() => {
+    const temperatureRanges: TemperatureRangesType[] = [
+      { max: -20, color: "bg-[#15195F]" },
+      { min: -20, max: -15, color: "bg-[#1C227E]" },
+      { min: -15, max: -10, color: "bg-[#2D36CB]" },
+      { min: -10, max: -5, color: "bg-[#2B69FD]" },
+      { min: -5, max: 0, color: "bg-[#2783EE]" },
+      { min: 0, max: 5, color: "bg-[#239DDE]" },
+      { min: 5, max: 10, color: "bg-[#27AABC]" },
+      { min: 10, max: 15, color: "bg-[#24C487]" },
+      { min: 15, max: 20, color: "bg-[#42CE4B]" },
+      { min: 20, max: 25, color: "bg-[#92C652]" },
+      { min: 25, max: 30, color: "bg-[#F9B234]" },
+      { min: 30, max: 35, color: "bg-[#DC721A]" },
+      { min: 35, max: 40, color: "bg-[#CE520D]" },
+      { min: 40, max: 45, color: "bg-[#BF3100]" },
+      { min: 45, color: "bg-[#841100]" },
+    ];
 
-  useEffect(() => {
-    let color = "";
+    const foundRange = temperatureRanges.find(
+      ({ min, max }) =>
+        tempData >= (min ?? -Infinity) && tempData <= (max ?? Infinity)
+    );
+    const color = foundRange ? foundRange.color : "bg-black";
 
-    switch (true) {
-      case tempData < -20:
-        color = "bg-[#15195F]";
-        break;
-      case tempData >= -20 && tempData < -15:
-        color = "bg-[#1C227E]";
-        break;
-      case tempData >= -15 && tempData < -10:
-        color = "bg-[#2D36CB]";
-        break;
-      case tempData >= -10 && tempData < -5:
-        color = "bg-[#2B69FD]";
-        break;
-      case tempData >= -5 && tempData < 0:
-        color = "bg-[#2783EE]";
-        break;
-      case tempData >= 0 && tempData <= 5:
-        color = "bg-[#239DDE]";
-        break;
-      case tempData > 5 && tempData <= 10:
-        color = "bg-[#27AABC]";
-        break;
-      case tempData > 10 && tempData <= 15:
-        color = "bg-[#24C487]";
-        break;
-      case tempData > 15 && tempData <= 20:
-        color = "bg-[#42CE4B]";
-        break;
-      case tempData > 20 && tempData <= 25:
-        color = "bg-[#92C652]";
-        break;
-      case tempData > 25 && tempData <= 30:
-        color = "bg-[#F9B234]";
-        break;
-      case tempData > 30 && tempData <= 35:
-        color = "bg-[#DC721A]";
-        break;
-      case tempData > 35 && tempData <= 40:
-        color = "bg-[#CE520D]";
-        break;
-      case tempData > 40 && tempData <= 45:
-        color = "bg-[#BF3100]";
-        break;
-      case tempData > 45:
-        color = "bg-[#841100]";
-        break;
-      default:
-        color = "bg-[#000]";
-        break;
-    }
-
-    setTempColor(color);
+    return color;
   }, [tempData]);
 
   return tempColor;
