f8574e3e9741137b19e6e4a39a36080f0faf59e2
Zombox <ghostzb@outlook.com>
2024-04-29 20:45:44 +0700

    delete TimeStampProvider and add CountryPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { PinProvider } from "./context/pin/PinProvider";
-import { TimeStampProvider } from "./context/dateTime/TimeStampProvider";
 import { WeatherProvider } from "./context/weather/WeatherProvider";
 import HomePage from "./pages/HomePage";
 import DateTimeCityPage from "./pages/DateTimeCityPage";
 import LoginPage from "./pages/LoginPage";
 import ErrorPage from "./pages/ErrorPage";
 import SignUpPage from "./pages/SignUpPage";
+import CountryPage from "./pages/CountryPage";
 
 function App() {
   return (
     <PinProvider>
-      <TimeStampProvider>
-        <WeatherProvider>
-          <Routes>
-            <Route path='login' element={<LoginPage />} caseSensitive />
-            <Route path='sign-up' element={<SignUpPage />} caseSensitive />
-            <Route path='/' index element={<HomePage />} caseSensitive />
-            <Route path='city' element={<Navigate to='city/thailand' replace />} />
-            <Route path='city/:cityPath' element={<DateTimeCityPage />} caseSensitive />
-            <Route path='*' index element={<ErrorPage />} caseSensitive />
-          </Routes>
-        </WeatherProvider>
-      </TimeStampProvider>
+      <WeatherProvider>
+        <Routes>
+          <Route path='/' index element={<HomePage />} caseSensitive />
+          <Route path='/country' element={<CountryPage />} caseSensitive />
+          <Route path='city' element={<Navigate to='city/thailand' replace />} />
+          <Route path='city/:cityPath' element={<DateTimeCityPage />} caseSensitive />
+          <Route path='login' element={<LoginPage />} caseSensitive />
+          <Route path='sign-up' element={<SignUpPage />} caseSensitive />
+          <Route path='*' index element={<ErrorPage />} caseSensitive />
+        </Routes>
+      </WeatherProvider>
     </PinProvider>
   );
 }
