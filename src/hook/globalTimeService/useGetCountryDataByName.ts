370d640798d1723d0232699eb32067029c19f907
Zombox <ghostzb@outlook.com>
2024-05-19 21:30:00 +0700

    add sortCountryData

--- src/hook/globalTimeService/useGetCountryDataByName.ts
+++ src/hook/globalTimeService/useGetCountryDataByName.ts
@@ -3,6 +3,7 @@ import { AppDispatch, RootType } from "../../redux/store";
 import { useEffect } from "react";
 import { fetchCountryDataByName } from "../../redux/country/getCountryDataByNameSlice";
 import { GetCountryDataByNamePropsType } from "../../service/country/getCountryDataByName";
+import { GlobalTimeDataType } from "../../service/GlobalTimeService.type";
 
 function useGetCountryDataByName({ countryName, page, size }: GetCountryDataByNamePropsType) {
   const dispatch = useDispatch<AppDispatch>();
@@ -12,8 +13,14 @@ function useGetCountryDataByName({ countryName, page, size }: GetCountryDataByNa
     dispatch(fetchCountryDataByName({ countryName, page, size }));
   }, [dispatch, countryName, page, size]);
 
+  const sortCountryData = data?.content
+    ? [...data.content].sort((a: GlobalTimeDataType, b: GlobalTimeDataType) =>
+        a.city_name.localeCompare(b.city_name),
+      )
+    : [];
+
   return {
-    data,
+    sortCountryData,
     loading,
     error,
   };
