c9ce4e32d50125746580cba4cb59d54192b988f2
Zombox <ghostzb@outlook.com>
2024-05-19 21:38:48 +0700

    Edit data according to the type of data.

--- src/pages/DateTimeCityPage.tsx
+++ src/pages/DateTimeCityPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import DisplayDateTime from "../components/containerDateTime/DisplayDateTime";
 import DisplayWeather from "../components/containerWeather/DisplayWeather";
-import ButtonNavigateToHome from "../components/ButtonNavigateToHome";
+// import ButtonNavigateToHome from "../components/ButtonNavigateToHome";
 import { useWeatherContext } from "../context/weather/WeatherContext";
 import useGetCityByCityName from "../hook/globalTimeService/useGetCityByCityName";
 import { formatForURL } from "../helper/formatForURL";
@@ -13,7 +13,7 @@ function DateTimeCityPage() {
   const { data } = useGetCityByCityName({ cityName: formatCityName });
   const { weatherRecords, addCity } = useWeatherContext();
 
-  const cityName = data?.city_name;
+  const cityName = data ? data[0].city_name : "";
   const findWeatherData = weatherRecords.some(
     (record) => record.name.toLowerCase() === cityName?.toLowerCase(),
   );
@@ -25,17 +25,17 @@ function DateTimeCityPage() {
   }, [cityName]);
 
   return (
-    <div className='h-dvh w-dvw overflow-hidden p-4 sm:p-8 md:p-12'>
+    <div>
       {/* <div className=' absolute left-0 top-0 z-[60] h-24 w-full bg-eerieBlack' /> */}
       <div className='grid h-[90dvh] grid-cols-6 grid-rows-3 sm:grid-cols-8'>
-        <div className='col-start-1 row-start-1 h-20 sm:col-end-2'>
+        {/* <div className='col-start-1 row-start-1 h-20 sm:col-end-2'>
           <ButtonNavigateToHome />
-        </div>
+        </div> */}
         <div
           className=' col-start-2 col-end-9 row-start-2 row-end-3 flex justify-center sm:col-start-3 sm:justify-end
           md:col-start-4 md:col-end-8 lg:col-start-5'
         >
-          {data && <DisplayDateTime data={data} />}
+          {data && <DisplayDateTime data={data[0]} />}
         </div>
         <div className=' col-start-1 row-start-3 row-end-4 hidden justify-end sm:col-span-9 sm:flex md:col-end-8 xl:col-start-3'>
           {findWeatherData && cityName && <DisplayWeather cityName={cityName} />}
