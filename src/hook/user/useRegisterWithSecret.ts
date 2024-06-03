a0fdc74f81d915a38b5795807c980a4dfb0a56ed
Zombox <ghostzb@outlook.com>
2024-06-03 18:36:42 +0700

    add useRegisterWithSecret for check Secret

---
+++ src/hook/user/useRegisterWithSecret.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { fetchGetRegister } from "../../redux/authentication/getRegisterSlice";
+
+function useRegisterWithSecret() {
+  const dispatch = useDispatch<AppDispatch>();
+  const secret = localStorage.getItem("secret");
+
+  const isMounted = useRef<boolean>(true);
+
+  const getRegisterWithSecret = useCallback(async () => {
+    try {
+      if (secret) {
+        const { email, jwt } = JSON.parse(secret);
+        await dispatch(fetchGetRegister({ email, jwt }));
+      } else {
+        throw new Error("Invalid secret format");
+      }
+    } catch (error) {
+      console.error("Error parsing secret or fetching register:", error);
+    }
+  }, [secret, dispatch]);
+
+  useEffect(() => {
+    if (secret && isMounted.current) {
+      getRegisterWithSecret();
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [secret, getRegisterWithSecret]);
+
+  return null;
+}
+
+export default useRegisterWithSecret;
