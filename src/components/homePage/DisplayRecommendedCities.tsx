386e53c5acb46e4fa1e0050acab8529d44324948
Zombox <ghostzb@outlook.com>
2024-06-03 18:33:25 +0700

    edit h2 to h3 and add props

--- src/components/homePage/DisplayRecommendedCities.tsx
+++ src/components/homePage/DisplayRecommendedCities.tsx
@@ -1,33 +1,35 @@
 import BoxCountryDateTime from "./BoxCountryDateTime";
-import useGetInterestGroupData from "../../hook/globalTimeService/useGetInterestGroupData";
+import { InterestDataType } from "../../service/interest/getInterestGroupData.type";
 
-function DisplayRecommendedCities() {
-  const { sortInterestData, loading, error } = useGetInterestGroupData({
-    page: 1,
-    size: 8,
-  });
+type DisplayRecommendedCitiesPropsType = {
+  sortInterestData: InterestDataType[];
+  loading: boolean;
+  error: string | null;
+};
+
+function DisplayRecommendedCities(props: DisplayRecommendedCitiesPropsType) {
+  const { sortInterestData, loading, error } = props;
 
   return (
     <div className='flex flex-col items-center justify-center'>
-      <div className='w-[90dvw] sm:max-w-[45rem] lg:max-w-[76rem] 2xl:w-[80dvw] 2xl:max-w-[85rem]'>
+      <div className='layout__content'>
         <div className=' mb-8 px-[25%] sm:px-8 md:px-16  xl:px-8 2xl:px-[2%]'>
           <h3>Recommended cities</h3>
         </div>
         {loading && (
           <div className='flex h-80 items-center justify-center'>
-            <h2>Loading...</h2>
+            <h3>Loading...</h3>
           </div>
         )}
         {error && (
           <div className='flex h-80 items-center justify-center'>
-            <h2>Error: {error}</h2>
+            <h3 className=' text-red-500'>Error: {error}</h3>
           </div>
         )}
         {!loading && !error && (
           <div
-            className='relative grid w-[90dvw] gap-y-5 sm:max-w-[45rem]
-            sm:grid-cols-2 sm:gap-y-8 lg:max-w-[76rem] lg:grid-cols-3 
-            xl:grid-cols-4 2xl:w-[80dvw] 2xl:max-w-[85rem]'
+            className='layout__content relative grid gap-y-5 sm:grid-cols-2 sm:gap-y-8 
+            lg:grid-cols-3 xl:grid-cols-4'
           >
             {sortInterestData.map((item, index) => (
               <div key={index} className=' flex items-center justify-center'>
