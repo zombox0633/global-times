8a848cf6ceb20fcd40b3c685ec437826e6e25a9d
Zombox <ghostzb@outlook.com>
2024-06-03 18:35:24 +0700

    edit design

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,21 +1,52 @@
+import { NavLink } from "react-router-dom";
 import AuthenticationForm from "../components/AuthenticationForm";
+import useAuthentication from "../hook/user/useAuthentication";
 
 function LoginPage() {
+  const { authStatus, isProcessing, handleInputChange, onAuthenticate } = useAuthentication();
+
   return (
     <>
       <AuthenticationForm isLogin={true}>
         <div className='min-h-80'>
-          <div className=' flex h-48 flex-col justify-between px-10'>
-            <input type='email' placeholder='Email address' className='authentication__input' />
+          <form onSubmit={onAuthenticate} className=' flex h-56 flex-col justify-between px-10'>
             <input
-              type='password'
-              placeholder='Password address'
+              type='email'
+              name='email'
+              placeholder='Email address'
+              min={12}
+              required
+              onChange={handleInputChange}
+              disabled={isProcessing}
               className='authentication__input'
             />
-            <button className='py-2 text-lg'>Continue</button>
-          </div>
-          <div className='mt-8 flex items-center justify-center'>
-            <p>Don't have an account?</p>
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              min={8}
+              required
+              onChange={handleInputChange}
+              disabled={isProcessing}
+              className='authentication__input'
+            />
+            <div className=' relative w-80'>
+              <button
+                type='submit'
+                disabled={isProcessing}
+                className='w-80 bg-eerieBlack py-2 text-lg'
+              >
+                Continue
+              </button>
+              <div className=' mr-1 mt-2 flex h-8 justify-end'>
+                <span className=' text-sm'>{authStatus}</span>
+              </div>
+            </div>
+          </form>
+          <div className='mt-10 flex items-center justify-center'>
+            <NavLink to={"/sign-up"}>
+              <p>Don't have an account?</p>
+            </NavLink>
           </div>
         </div>
       </AuthenticationForm>
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import AuthenticationForm from "../components/AuthenticationForm";
 
 function SignUpPage() {
@@ -6,17 +7,38 @@ function SignUpPage() {
       <AuthenticationForm isLogin={false}>
         <div className='min-h-80'>
           <div className=' flex h-60 flex-col justify-between px-10'>
-            <input type='email' placeholder='Email' className='authentication__input' />
-            <input type='password' placeholder='Password' className='authentication__input' />
+            <input
+              type='email'
+              name='email'
+              min={12}
+              required
+              placeholder='Email'
+              className='authentication__input'
+            />
             <input
               type='password'
+              name='password'
+              min={8}
+              required
+              placeholder='Password'
+              className='authentication__input'
+            />
+            <input
+              type='password'
+              name='confirmPassword'
+              min={8}
+              required
               placeholder='Confirm Password'
               className='authentication__input'
             />
-            <button className='py-2 text-lg'>Continue</button>
+            <button type='submit' className='bg-eerieBlack py-2 text-lg'>
+              Continue
+            </button>
           </div>
           <div className='mt-8 flex items-center justify-center'>
-            <p>Don't have an account?</p>
+            <NavLink to={"/login"}>
+              <p>Already have an account?</p>
+            </NavLink>
           </div>
         </div>
       </AuthenticationForm>
