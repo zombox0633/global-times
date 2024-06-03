7c83e7bc9d11c50386f4578f64b348e1be01cace
Zombox <ghostzb@outlook.com>
2024-06-03 18:25:21 +0700

    change name function

--- src/helper/formatForURL.ts
+++ src/helper/formatForURL.ts
@@ -1,4 +1,4 @@
-export const firstLetterUppercase = (text: string): string => {
+export const capitalizeFirstLetter = (text: string): string => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
 
