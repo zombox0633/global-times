d14401b0585dffe990dd623a4ea3b0355046e320
Zombox <ghostzb@outlook.com>
2024-06-03 18:39:50 +0700

    add useGetInterestGroupData in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,26 @@ import DisplayPinsCity from "../components/homePage/DisplayPinsCity";
 import DisplayRecommendedCities from "../components/homePage/DisplayRecommendedCities";
 import HeaderSection from "../components/searchBar/HeaderSection";
 import { usePinContext } from "../context/pin/PinContext";
+import useGetInterestGroupData from "../hook/globalTimeService/useGetInterestGroupData";
 
 function HomePage() {
+  const { sortInterestData, loading, error } = useGetInterestGroupData({
+    page: 1,
+    size: 8,
+  });
   const { pins } = usePinContext();
   const hasPin: boolean = pins.length > 0;
+  const hasSortInterestData: boolean = sortInterestData.length > 0;
 
   return (
-    <div>
+    <div className='mb-12 2xl:mb-20'>
       <HeaderSection />
-      {hasPin && <DisplayPinsCity pins={pins} />}
-      <DisplayRecommendedCities />
-      <div className='h-8 2xl:h-20' />
+      {hasPin && hasSortInterestData && <DisplayPinsCity pins={pins} />}
+      <DisplayRecommendedCities
+        sortInterestData={sortInterestData}
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 }
