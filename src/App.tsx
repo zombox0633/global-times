47c331500c7df72add62e6fe67a2780ba0636c6f
Zombox <ghostzb@outlook.com>
2024-06-09 17:08:49 +0700

    add DataLoadingScreen and UniversalLoader to app

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,32 @@ import CountryPage from "./pages/CountryPage";
 import Layout from "./components/Layout";
 import ContinentPage from "./pages/ContinentPage";
 import useRegisterWithSecret from "./hook/user/useRegisterWithSecret";
+import UniversalLoader from "./components/loader/UniversalLoader";
 
 function App() {
   useRegisterWithSecret();
 
   return (
-    <PinProvider>
-      <WeatherProvider>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<HomePage />} caseSensitive />
-            <Route path='continent' element={<Navigate to='continent/america' replace />} />
-            <Route path='continent/:continentPath' element={<ContinentPage />} caseSensitive />
-            <Route path='country' element={<Navigate to='country/united_states' replace />} />
-            <Route path='country/:countryPath' element={<CountryPage />} caseSensitive />
-            <Route path='city' element={<Navigate to='city/new_york' replace />} />
-            <Route path='city/:cityPath' element={<DateTimeCityPage />} caseSensitive />
-          </Route>
-          <Route path='login' element={<LoginPage />} caseSensitive />
-          <Route path='sign-up' element={<SignUpPage />} caseSensitive />
-          <Route path='*' index element={<ErrorPage />} caseSensitive />
-        </Routes>
-      </WeatherProvider>
-    </PinProvider>
+    <UniversalLoader>
+      <PinProvider>
+        <WeatherProvider>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<HomePage />} caseSensitive />
+              <Route path='continent' element={<Navigate to='continent/america' replace />} />
+              <Route path='continent/:continentPath' element={<ContinentPage />} caseSensitive />
+              <Route path='country' element={<Navigate to='country/united_states' replace />} />
+              <Route path='country/:countryPath' element={<CountryPage />} caseSensitive />
+              <Route path='city' element={<Navigate to='city/new_york' replace />} />
+              <Route path='city/:cityPath' element={<DateTimeCityPage />} caseSensitive />
+            </Route>
+            <Route path='login' element={<LoginPage />} caseSensitive />
+            <Route path='sign-up' element={<SignUpPage />} caseSensitive />
+            <Route path='*' index element={<ErrorPage />} caseSensitive />
+          </Routes>
+        </WeatherProvider>
+      </PinProvider>
+    </UniversalLoader>
   );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "./navigation/NavigationBar";
+import DataLoadingScreen from "./loader/DataLoadingScreen";
 
 function Layout() {
   return (
     <div className='relative flex h-dvh min-h-dvh flex-col justify-between overflow-x-hidden'>
       <NavigationBar />
       <div className=' flex-grow'>
+        <DataLoadingScreen />
         <Outlet />
       </div>
     </div>
