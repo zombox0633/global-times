fa75097a2adc3c51c4d3b19d216dd72956ecd11b
Zombox <ghostzb@outlook.com>
2024-06-09 16:57:40 +0700

    Refactor LoginPage to remove unnecessary imports and simplify input change handling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,54 +1,46 @@
-import { NavLink } from "react-router-dom";
 import AuthenticationForm from "../components/AuthenticationForm";
 import useAuthentication from "../hook/user/useAuthentication";
 
 function LoginPage() {
-  const { authStatus, isProcessing, handleInputChange, onAuthenticate } = useAuthentication();
+  const { authStatus, isProcessing, handleChange, onAuthenticate } = useAuthentication();
 
   return (
     <>
       <AuthenticationForm isLogin={true}>
-        <div className='min-h-80'>
-          <form onSubmit={onAuthenticate} className=' flex h-56 flex-col justify-between px-10'>
-            <input
-              type='email'
-              name='email'
-              placeholder='Email address'
-              min={12}
-              required
-              onChange={handleInputChange}
+        <form onSubmit={onAuthenticate} className=' flex h-56 flex-col justify-between px-10'>
+          <input
+            type='email'
+            name='email'
+            placeholder='Email address'
+            min={12}
+            required
+            onChange={handleChange}
+            disabled={isProcessing}
+            className='authentication__input'
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            min={8}
+            required
+            onChange={handleChange}
+            disabled={isProcessing}
+            className='authentication__input'
+          />
+          <div className=' relative w-80'>
+            <button
+              type='submit'
               disabled={isProcessing}
-              className='authentication__input'
-            />
-            <input
-              type='password'
-              name='password'
-              placeholder='Password'
-              min={8}
-              required
-              onChange={handleInputChange}
-              disabled={isProcessing}
-              className='authentication__input'
-            />
-            <div className=' relative w-80'>
-              <button
-                type='submit'
-                disabled={isProcessing}
-                className='w-80 bg-eerieBlack py-2 text-lg'
-              >
-                Continue
-              </button>
-              <div className=' mr-1 mt-2 flex h-8 justify-end'>
-                <span className=' text-sm'>{authStatus}</span>
-              </div>
+              className='w-80 bg-eerieBlack py-2 text-lg'
+            >
+              Continue
+            </button>
+            <div className=' mr-1 mt-2 flex h-8 justify-end'>
+              <span className=' text-sm'>{authStatus}</span>
             </div>
-          </form>
-          <div className='mt-10 flex items-center justify-center'>
-            <NavLink to={"/sign-up"}>
-              <p>Don't have an account?</p>
-            </NavLink>
           </div>
-        </div>
+        </form>
       </AuthenticationForm>
     </>
   );
