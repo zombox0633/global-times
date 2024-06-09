9af2f934f105a1996b671ce0bd137a25e6a46244
Zombox <ghostzb@outlook.com>
2024-06-09 17:01:40 +0700

    Update AuthenticationForm to conditionally display background and welcome message based on authentication status

--- src/components/AuthenticationForm.tsx
+++ src/components/AuthenticationForm.tsx
@@ -1,14 +1,23 @@
 import { PropsWithChildren } from "react";
 import ButtonNavigateToHome from "./ButtonNavigateToHome";
+import clsx from "clsx";
+import { NavLink } from "react-router-dom";
 
 type AuthenticationFormPropsType = {
   isLogin: boolean;
 };
 
 function AuthenticationForm({ isLogin, children }: PropsWithChildren<AuthenticationFormPropsType>) {
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+
   return (
     <div className=' grid h-dvh w-dvw grid-cols-2 lg:grid-cols-[60%_40%]'>
-      <div className='bg__gradient hidden pl-6 pt-6 md:block'>
+      <div
+        className={clsx("hidden pl-6 pt-6 md:block", {
+          bg__grad1: isLogin,
+          bg__grad2: !isLogin,
+        })}
+      >
         <ButtonNavigateToHome />
       </div>
       <div className='col-start-1 col-end-3 bg-black px-5 pt-8 md:col-start-2'>
@@ -16,13 +25,22 @@ function AuthenticationForm({ isLogin, children }: PropsWithChildren<Authenticat
           <div className=' flex h-[30rem] w-[25rem] flex-col justify-between'>
             <div className=' h-24 px-10 py-8'>
               <div className=' mb-2'>
-                <h2 className=' text-center text-5xl lg:text-4xl'>Welcome</h2>
+                <h2 className=' text-center text-5xl lg:text-4xl'>
+                  Welcome {isAuthenticated ? "back" : ""}
+                </h2>
               </div>
               <p className=' text-center text-lg'>
                 {isLogin ? "Please login to continue" : "Create a new account to get started"}
               </p>
             </div>
-            {children}
+            <div className="className='min-h-80'">
+              {children}
+              <div className='mt-10 flex items-center justify-center'>
+                <NavLink to={isLogin ? "/sign-up" : "/login"}>
+                  <p>{isLogin ? "Don't have an account?" : "Already have an account?"}</p>
+                </NavLink>
+              </div>
+            </div>
           </div>
         </div>
       </div>
