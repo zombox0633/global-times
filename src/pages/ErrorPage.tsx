0a8e96d16247579edef88d0b0366448780b771c4
Zombox <ghostzb@outlook.com>
2024-06-09 16:58:09 +0700

    Enhance ErrorPage to display dynamic error codes and messages using useLocation

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,12 +1,21 @@
+import { useLocation } from "react-router-dom";
+
 function ErrorPage() {
+  const location = useLocation();
+  const code = location.state?.code || 404;
+  const message =
+    code === 404
+      ? "The page you are looking for doesn't exist."
+      : "Service Unavailable. Please try again later.";
+
   return (
     <div className=' grid h-dvh w-dvw justify-between p-4 sm:p-8 md:p-12'>
-      <span className=' text-xl'>404</span>
+      <span className=' text-xl'>{code}</span>
       <div>
         <div className=' my-8'>
-          <h1>Page not found</h1>
+          <h1>{code === 404 ? "Page not found" : "Service Unavailable"}</h1>
         </div>
-        <p className='text-xl'>The page you are looking for doesn't exist.</p>
+        <p className='text-xl'>{message}</p>
       </div>
     </div>
   );
